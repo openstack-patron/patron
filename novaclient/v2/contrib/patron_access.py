1cab6ce5235bf3ce333337e60a27c3f88f1c5437
lwyeluo <lwyeluo@163.com>
2015-08-26 14:28:00 +0800

    added patron client into novaclient for testing

---
+++ novaclient/v2/contrib/patron_access.py
@@ -0,0 +1,49 @@
+# Copyright 2013 Rackspace Hosting
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from novaclient import base
+from novaclient.i18n import _
+from novaclient.openstack.common import cliutils
+from novaclient import utils
+
+
+class PatronResource(base.Resource):
+    def __repr__(self):
+        return "<PatronResource: %s>" % self.name
+
+
+class PatronAccessManager(base.Manager):
+    resource_class = PatronResource
+
+    def verify(self, user_id, resource_id):
+        """
+        patron verify
+        """
+        # os-patron-access/123/resource/456/action/verify
+        return self.api.client.get("/os-patron-access/%s/resource/%s/action/verify" % (user_id, resource_id))
+
+
+@cliutils.arg(
+    '--user_id',
+    metavar='<user_id>',
+    help=_('User id.'))
+@cliutils.arg(
+    '--resource_id',
+    metavar='<resource_id>',
+    help=_('Resource id.'))
+def do_verify(cs, args):
+    """patron verify"""
+    ans = cs.patron_access.verify(args.user_id, args.resource_id)
+    print ans
